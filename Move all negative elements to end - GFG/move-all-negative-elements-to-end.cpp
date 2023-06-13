72fb2a3180421bc6bc29620c0fade209b4437ebc
Ajay Kumar <108025008+ak85846@users.noreply.github.com>
2023-06-13 14:56:46 +0530

    Added solution - LeetHub

---
+++ Move all negative elements to end - GFG/move-all-negative-elements-to-end.cpp
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    void segregateElements(int arr[],int n)
+    {
+        // Your code goes here
+      /* for not having order and no extra space
+      int left=0,right=n-1;
+        while(left<right){
+            if(arr[left]>0 && arr[right]<0)
+            left++,right--;
+            else if(arr[left]<0 && arr[right]>0){
+                swap(arr[left],arr[right]);
+                left++,right--;
+            }
+            else if(arr[left]<0 && arr[right] <0){
+                right--;
+            }
+            else
+           left++;
+        }
+        */
+        /* using extra space*/
+        int temp[n],j=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]>=0){
+                temp[j++]=arr[i];
+            }
+        }
+        if(j==n || j==0)
+        return;
+        for(int i=0;i<n;i++){
+            if(arr[i]<0){
+                temp[j++]=arr[i];
+            }
+        }
+     for(int i=0;i<n;i++){
+         arr[i]=temp[i];
+     }   
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		int a[n];
+		for(int i=0;i<n;i++)
+		cin>>a[i];
+		Solution ob;
+		ob.segregateElements(a,n);
+		
+        for(int i=0;i<n;i++)
+        cout<<a[i]<<" ";
+		cout<<endl;
+	}
+}
+
+// } Driver Code Ends
